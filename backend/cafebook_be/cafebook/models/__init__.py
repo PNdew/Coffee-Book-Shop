36c74749a04daf7044ec2b8d35ba4ff33adb6f44
HaHmii <loveyouhateyou123456@gmail.com>
2025-05-26 21:52:54 +0700

    #commit trước khi merge

--- backend/cafebook_be/cafebook/models/__init__.py
+++ backend/cafebook_be/cafebook/models/__init__.py
@@ -1,8 +1,8 @@
 from .user import NhanVien, TaiKhoan
 from .product import Sanpham
 from .voucher import Voucher
-from .invoiceline import Donghoadon
-from .invoice import Hoadon
+from .orderline import Donghoadon
+from .order import Hoadon
 from .ingredient import NguyenLieu
 from .book import Sach
 from .attendance import Attendance
--- backend/cafebook_be/cafebook/urls.py
+++ backend/cafebook_be/cafebook/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views.authentication import login_view  # Import hàm xử lý login
-from .views.createOrder import createOrder, createOrderDetails
+from .views.order import createOrder, createOrderDetails, confirmPayment
 from .views.getProduct import (SanphamViewSet, VoucherViewSet, DonghoadonViewSet, HoadonViewSet)  # Import các viewset cho sản phẩm, voucher và hóa đơn
 from .views.ingredient import NguyenLieuViewSet  # Import viewset cho nguyên liệu
 from .views.book import SachViewSet, TheLoaiViewSet  # Import viewset cho sách
@@ -22,6 +22,7 @@ urlpatterns = [
     path("attendance/check/", check_attendance, name="check_attendance"),
     path("order/create/", createOrder, name="create_order"),
     path("order/details/", createOrderDetails, name="create_order_line"),
+    path("order/payment-confirmation/", confirmPayment, name="confirm_payment"),
     path("", include(router.urls)),
     path('auth/login/', LoginView.as_view(), name='login'),
 ]
